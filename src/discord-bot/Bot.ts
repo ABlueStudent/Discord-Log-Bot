56b1161b9529f7b19914278c01b10493819ad3bc
cute_panda <asdf3601a@gmail.com>
2021-05-24 09:27:19 +0800

    waiting mongodb

--- src/discord-bot/Bot.ts
+++ src/discord-bot/Bot.ts
@@ -5,19 +5,10 @@ export class Bot {
 
     constructor(token: string) {
         this.token = token;
-        
         let bot = Eris(this.token)
-        bot.on("messageCreate", (msg) => {
-            if (msg.content == "!ping") {
-                bot.createMessage(msg.channel.id, "Pong!")
-            }
-            console.log("["+new Date(msg.timestamp).toTimeString()+"]"+msg.author.username+":"+msg.content)
-            // console.log(msg)
-        })
 
-        bot.on("messageUpdate", (msg) => {
-            console.log("["+new Date(msg.timestamp).toTimeString()+"]"+msg.author.username+":"+msg.content)
-        })
+        bot.on("messageCreate", (res) => {})
+        bot.on("messageUpdate", (res) => {})
 
         bot.connect()
     }
